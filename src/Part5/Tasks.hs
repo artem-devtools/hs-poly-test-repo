d0463d89565285985ffcc66dc26c3a403c58318c
ArtemUntila <artem.devtools@gmail.com>
2024-01-07 00:05:10 +0300

    Part5

--- src/Part5/Tasks.hs
+++ src/Part5/Tasks.hs
@@ -4,29 +4,31 @@ import Util(notImplementedYet)
 
 -- Реализуйте левую свёртку
 myFoldl :: (b -> a -> b) -> b -> [a] -> b
-myFoldl = notImplementedYet
+myFoldl _ i [] = i
+myFoldl f i (x:xs) = myFoldl f (f i x) xs 
 
 -- Реализуйте правую свёртку
 myFoldr :: (a -> b -> b) -> b -> [a] -> b
-myFoldr = notImplementedYet
+myFoldr _ i [] = i
+myFoldr f i (x:xs) = f x (myFoldr f i xs)
 
 -- Используя реализации свёрток выше, реализуйте все остальные функции в данном файле
 
 myMap :: (a -> b) -> [a] -> [b]
-myMap f = notImplementedYet
+myMap f lst = myFoldr (\e acc -> (f e):acc) [] lst
 
 myConcatMap :: (a -> [b]) -> [a] -> [b]
-myConcatMap f = notImplementedYet
+myConcatMap f lst = myFoldl (\acc e -> acc ++ (f e)) [] lst
 
 myConcat :: [[a]] -> [a]
-myConcat = notImplementedYet
+myConcat lst2D = myFoldl (\acc lst -> acc ++ lst) [] lst2D
 
 myReverse :: [a] -> [a]
-myReverse = notImplementedYet
+myReverse lst = myFoldl (\acc e -> e:acc) [] lst
 
 myFilter :: (a -> Bool) -> [a] -> [a]
-myFilter p = notImplementedYet
+myFilter p lst = myFoldr (\e acc -> if p e then e:acc else acc) [] lst
 
 myPartition :: (a -> Bool) -> [a] -> ([a], [a])
-myPartition p = notImplementedYet
+myPartition p lst = myFoldr (\e (accTrue, accFalse) -> if p e then (e:accTrue, accFalse) else (accTrue, e:accFalse)) ([], []) lst
 
