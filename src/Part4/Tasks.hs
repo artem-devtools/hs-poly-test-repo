44341bedd412da65dcee68af6edb7c91aff1d5e4
ArtemUntila <artem.devtools@gmail.com>
2024-01-08 22:38:57 +0300

    Part4

--- src/Part4/Tasks.hs
+++ src/Part4/Tasks.hs
@@ -16,17 +16,50 @@ rlistToList lst =
 
 -- Реализуйте обратное преобразование
 listToRlist :: [a] -> ReverseList a
-listToRlist = notImplementedYet
+listToRlist list = l2r $ reverse list
+    where l2r [] = REmpty
+          l2r (x:xs) = (l2r xs) :< x 
 
 -- Реализуйте все представленные ниже классы (см. тесты)
-instance Show (ReverseList a) where
-    showsPrec = notImplementedYet
-    show = notImplementedYet
-instance Eq (ReverseList a) where
-    (==) = notImplementedYet
-    (/=) = notImplementedYet
+instance (Show a) => Show (ReverseList a) where
+  --show :: ReverseList a -> String
+    show REmpty = "[]"
+    show rlist = "[" ++ (join rlist) ++ "]"
+        where join (REmpty :< x) = show x
+              join (xs :< x) = (join xs) ++ "," ++ (show x)
+    --showsPrec = notImplementedYet
+
+instance (Eq a) => Eq (ReverseList a) where
+  --(==) :: ReverseList a -> ReverseList a -> Bool
+    (==) REmpty REmpty = True
+    (==) (as :< a) (bs :< b) = (a == b) && (as == bs)
+    (==) _ _ = False
+    --(/=) = notImplementedYet
+
 instance Semigroup (ReverseList a) where
+  --(<>) :: ReverseList a -> ReverseList a -> ReverseList a
+    (<>) as REmpty = as
+    (<>) REmpty bs = bs
+    (<>) as (bs :< b) = (as <> bs) :< b
+
 instance Monoid (ReverseList a) where
+  --mempty :: ReverseList a  
+    mempty = REmpty
+
 instance Functor ReverseList where
+  --fmap :: (a -> b) -> ReverseList a -> ReverseList b
+    fmap _ (REmpty) = REmpty
+    fmap mf (xs :< x) = (fmap mf xs) :< (mf x)
+
 instance Applicative ReverseList where
+  --pure :: a -> ReverseList a
+    pure x  = REmpty :< x
+  --(<*>) :: ReverseList (a -> b) -> ReverseList a -> ReverseList b
+    (<*>) REmpty _ = REmpty
+    (<*>) _ REmpty = REmpty 
+    (<*>) (fs :< f) as = (fs <*> as) <> (f <$> as)
+
 instance Monad ReverseList where
+  --(>>=) :: ReverseList a -> (a -> ReverseList b) -> ReverseList b
+    (>>=) REmpty _ = REmpty
+    (>>=) (as :< a) farb = (as >>= farb) <> (farb a)
